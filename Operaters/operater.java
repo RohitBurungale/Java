6a23e932e796237f09cd5b4d1bbf4fdc32ec13df
Rohit <rohitburungale5041@gmail.com>
2025-11-18 22:11:57 +0530

    Operaters in java

---
+++ Operaters/operater.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class operater {
+    public static void main(String[] args) {
+        
+    
+    int  a=19; 
+    int s=17;
+        int b=a++;
+        int c=++s;
+        System.out.println(b);
+        System.out.println(a);
+        System.out.println(c);
+        System.out.println(s);
+    }
+
+}
